353e70870bba5b70379f44c07f1a09e913c6e432
malefaro <malefaro@gmail.com>
2020-03-02 13:05:53 +0300

    missing type

--- cmd/promotionerAPI/models/foodplace.go
+++ cmd/promotionerAPI/models/foodplace.go
@@ -1,16 +1,18 @@
 package models
 
+import "promotioner/cmd/promotionerAPI/models/enums"
+
 type FoodPlace struct {
-	FoodPlaceID int64 `json:"food_place_id"`
-	Name        string `json:"name"`
-	PriceType   int16 `json:"price_type"`
-	Description string `json:"description"`
-	FoodPlaceType
+	FoodPlaceID   int64  `json:"food_place_id"`
+	Name          string `json:"name"`
+	PriceType     int16  `json:"price_type"`
+	Description   string `json:"description"`
+	FoodPlaceType enums.FoodPlaceType
 	// TODO: other fields
 }
 
 type FoodPlaceAddress struct {
-	FoodPlaceAddressID int64 `json:"food_place_address_id"`
+	FoodPlaceAddressID int64   `json:"food_place_address_id"`
 	Latitude           float64 `json:"latitude"`
 	Longitude          float64 `json:"longitude"`
 	// foreign keys
